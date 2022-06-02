500e387aa7f8dec3f12d45a14a940fbc100a74ec
Marshall Zmuda <marshall.z@outlook.com>
2022-06-02 14:47:06 -0700

    Implement functional queue and stack w/ 0 index

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,26 +1,42 @@
 var Queue = function() {
+  //used to house the methods associated with a queue object
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
-
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    var key = Object.keys(storage).length;
+    //uses keys function to identify next numeric key to add value at
+    var currentKeys = Object.keys(storage);
+    var key = Number(currentKeys[currentKeys.length - 1]) + 1;
     storage[key] = value;
-    console.log(key);
+
+    //* ADDS TIME COMPLEXITY BUT KEEPS KEYS 0-INDEXED */
+    var newStorage = {};
+    for (var i = 0; i < this.size(); i++) {
+      newStorage[i] = Object.values(storage)[i];
+    }
+    storage = newStorage;
   };
 
   someInstance.dequeue = function() {
+    /* if used 0-index method */
+    // var retVal = storage[0];
+    // delete storage[0];
+    // return retVal;
+
+    //stores the last value
     var first = Object.keys(storage)[0];
     var firstProp = storage[first];
+    //deletes property, the returns stored value
     delete storage[first];
     return firstProp;
   };
 
   someInstance.size = function() {
-    return Object.keys(storage).length;
+    //returns the length of an array made from Object properties
+    return Object.entries(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //uses key function to determine numeric key at which to add the value
+    var key = Object.keys(storage).length;
+    storage[key] = value;
   };
 
   someInstance.pop = function() {
+    //identifies last property and stores it
+    var last = Object.keys(storage).length - 1;
+    var retVal = storage[last];
+    //deletes property and returns value
+    delete storage[last];
+    return retVal;
   };
 
   someInstance.size = function() {
+    //returns the length of array made from storage keys
+    return Object.keys(storage).length;
   };
 
   return someInstance;
