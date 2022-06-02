5a42689c79425af67c426fcc21fa5b3373f9c693
Marshall Zmuda <marshall.z@outlook.com>
2022-06-02 16:03:56 -0700

    Implement functional-shared for queue/stack.js

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
+  //creates an object which will be given all the shared methods.
+  var newQueue = {storage: {}};
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+queueMethods = {
+};
 
+queueMethods.enqueue = function(value) {
+  //newQueue will be calling these methods and is specified by 'this' keyword
+  var keys = Object.keys(this.storage);
+  var nextKey = keys[keys.length - 1] + 1 || 0;
+  this.storage[nextKey] = value;
+};
 
+queueMethods.dequeue = function() {
+  var keys = Object.keys(this.storage);
+  var firstKey = keys[0];
+  var retVal = this.storage[firstKey];
+  delete this.storage[firstKey];
+  return retVal;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {
+    storage: {}
+  };
+
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  var key = Object.keys(this.storage).length; //finds the length of the storage object to identify next key (0)
+  this.storage[key] = value; //Store value at last key (0)
+};
 
+stackMethods.pop = function() {
+  var keys = Object.keys(this.storage);
+  //identifys the last key, and then uses that key to access the property in storage
+  var lastKey = keys[keys.length - 1];
+  //stores the value, deletes it from the object, and then returns it
+  var retVal = this.storage[lastKey];
+  delete this.storage[lastKey];
+  return retVal;
+};
+
+stackMethods.size = function() {
+  //returns size based on the number of keys
+  return Object.keys(this.storage).length;
+};
