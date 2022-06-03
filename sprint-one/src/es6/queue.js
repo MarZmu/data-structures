55c289fbc2bf7ee2cb58f9733d5ce9dd4da1be2b
Marshall Zmuda <marshall.z@outlook.com>
2022-06-03 08:46:03 -0700

    Finish Implementation of ES6 stack/queue.js

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    var nextKey = Object.keys(this.storage).length;
+    this.storage[nextKey] = value;
+  }
+
+  dequeue() {
+    var retVal = this.storage[0];
+    delete this.storage[0];
+
+    var newStorage = {};
+    for (let i = 0; i < this.size(); i++) {
+      newStorage[i] = Object.values(this.storage)[i];
+    }
+    this.storage = newStorage;
+    return retVal;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,22 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    var nextKey = Object.keys(this.storage).length;
+    this.storage[nextKey] = value;
+  }
+
+  pop() {
+    var last = Object.keys(this.storage).length - 1;
+    var retVal = this.storage[last];
+    delete this.storage[last];
+    return retVal;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
