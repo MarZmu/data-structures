4ef518db0d98bebfe30222d7e3b8f87f6f3c7cac
Marshall Zmuda <marshall.z@outlook.com>
2022-06-03 15:28:31 -0700

    Implement linkedList.js and tree.js BRs

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,71 @@
 var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
+  var list = {}; // list to add nodes to
+  list.head = null; //node
+  list.tail = null; //node
 
-  list.addToTail = function(value) {
+  list.addToTail = function(value) { // takes a value and adds it to the end of the list
+    //create a node
+    var newTail = Node(value);
+    //next property  would be null
+
+    //make old tails next property points to the next node object
+    if (list.tail) {
+      list.tail.next = newTail;
+    }
+    // if head does not exits
+    if (!list.head) {
+      // reassign head to current tail
+      list.head = newTail;
+    }
+    //reassign list tail to current node
+    list.tail = newTail;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function() { // removes the first node from the list and returns its value, BIG 0: CONSTANT
+    // create temp var for head value
+    var retValue = list.head.value;
+    // store old heads next property to become new head
+    var newHead = list.head.next;
+    // delete old head node
+    delete list.head;
+    // assign list.head to the node at current heads next property
+    list.head = newHead;
+    // returns heads value
+    return retValue;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target) { // returns boolean reflecting whether or not the passed-in value is in the linked list  BIG 0: LINEAR
+    // I target value
+    // O boolean value (true if taget is in, else false)
+    // C
+    // E
+    // P
+    // create check value function
+    var checkValue = function (head) {
+      // I - node
+      // O - boolean val
+      // C
+      // E - input could be null, or another node
+      // P
+      //if value match
+      if (head.value === target) {
+        //return true
+        return true;
+      //check if next is null
+      } else if (!head.next) {
+        return false;
+      }
+      //go to next node and repeat
+      return checkValue(head.next);
+    };
+    // return inner function return value
+    return checkValue(list.head);
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function(value) { //creates node object with value and reference
   var node = {};
 
   node.value = value;
@@ -25,5 +75,21 @@ var Node = function(value) {
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
- */
+* Complexity: What is the time complexity of the above functions?
+ removeHead method: constant BIG O(1)
+ contains methdod: linear BIG O(n)
+*/
+
+
+
+//extra credits:
+
+// list.insert = function(reference) {
+//   //create node, make prior node point to this reference, point this node to next node
+// }
+
+// list.remove = function() {
+//   //if head just move head reference to next item
+//   //if middle - look up to find value that precedes target node
+// }
+// };
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  // extend treemethods on to newtree
+  _.extend(newTree, treeMethods);
+  newTree.children = []; // fix me  --an array containing a number of subtrees (can be many children not just left and right)
 
   return newTree;
 };
@@ -11,13 +13,52 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //takes any value, sets that as the target of a node, and adds that node as a child of the tree
+  //create a newTree by invoking Tree function
+  var child = Tree();
+  //give it input value for value property
+  child.value = value;
+  //push the child into 'this'.children
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  //takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+
+  // 'this' -- tree object that is calling contains left of the dot
+  var tree = this;
+  // I - target value
+  // O - boolean value
+
+  //default boolean false for found
+  var found = false;
+  // create a recursion function to loop through entire tree children
+  var searchTree = function(tree) {
+    // I - tree
+    // O - boolean value
+    // E - if input tree doesnt exist or no children, multiple children
+
+    // if value in tree matches
+    if (tree.value === target) {
+      // reassign found
+      found = true;
+      // if tree has children
+    } else if (tree.children.length > 0) {
+      // loop through each child
+      for (let i = 0; i < tree.children.length; i++) {
+        //invoke recursive function on each child
+        searchTree(tree.children[i]);
+      }
+    }
+  };
+  //inovke recursion function on main tree
+  searchTree(tree);
+  return found;
+  //linear time - requires visiting every single node (recursion)
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+   addChild - O(1)
+   contains - O(n)
  */
